efeb47fcb74ea4bc4325bfce5cae5fb912e8a27c
Trevor Jacklitch <trevorjacklitch123@gmail.com>
2019-02-15 18:54:36 -0600

    Added products to graphql api

--- app.js
+++ app.js
@@ -1,13 +1,64 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const graphQL = require('graphql');
+const { buildSchema } = require('graphql');
+const graphQLHttp = require('express-graphql');
 
 const app = express();
 
+const products = [];
+
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.json("Hello World");
-})
+app.use(
+    '/graphql',
+    graphQLHttp({
+        schema: buildSchema(`
+            type Product {
+                _id: ID!
+                name: String!
+                description: String!
+                price: Float!
+                quantity: Int!
+            }
+
+            input ProductInput {
+                name: String!
+                description: String!
+                price: Float!
+                quantity: Int!
+            }
+            
+            type RootQuery {
+                products: [Product!]!
+            }
+
+            type RootMutation {
+                createProduct(productInput: ProductInput): Product
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            products: () => {
+                return products;
+            },
+            createProduct: (args) => {
+                const product = {
+                    _id: Math.random() * 100,
+                    name: args.productInput.name,
+                    description: args.productInput.description,
+                    price: args.productInput.price,
+                    quantity: args.productInput.quantity
+                };
+                products.push(product);
+                return product;
+            }
+        },
+        graphiql: true
+    })
+);
 
 app.listen(4000);
